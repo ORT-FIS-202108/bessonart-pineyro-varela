064177bc0618dd3d90daa0b3695ba6afe7b88cbc
SofIa Piñeyro <sofiapineyro2302@hotmail.com>
2021-11-21 16:57:01 -0300

    Fix: agregarDeuda en grupo y balancear

--- dominio/grupo.js
+++ dominio/grupo.js
@@ -1,3 +1,5 @@
+import Deuda from '../../dominio/deuda.js';
+
 class Grupo {
 	constructor(nombre,listaIntegrantes) {
 		this.nombre = nombre;
@@ -54,29 +56,86 @@ class Grupo {
 		return -1;
 	}
 
-	agregarDeuda(deuda) {
-		this.listaDeudas.push(deuda);
-		this.balancear();
+	agregarDeuda(nombre, monto) {
+		monto /= this.cantidadIntegrantes();
+		let tieneDeudas = this.getDeudaDe(nombre);
+		if(tieneDeudas !== -1) {
+			let deudaNombre = this.listaDeudas[getDeudaDe(nombre)];
+			let amigos = deudaNombre.getAmigos();
+			let montos = deudaNombre.getMontos();
+			for(let i = 0; i < amigos.length; i++) {
+				if(montos[i] > monto) {
+					montos[i] -= monto;
+				} else if(montos[i] < monto) {
+					deudaNombre.eliminarDeuda(i);
+					let pos = this.getDeudaDe(amigos[i]);
+					if(pos !== -1) {
+						this.listaDeudas[pos].agregarDeuda(nombre, monto);
+					} else {
+						let nuevaDeuda = new Deuda(amigos[i], [nombre], [monto]);
+						this.listaDeudas.push(nuevaDeuda);
+					}
+				} else {
+					deudaNombre.eliminarDeuda(i);
+				} 
+			}
+			for(let amigo in this.getListaIntegrantes()) {
+				if(!(amigos.include(amigo))){
+					let pos = this.getDeudaDe(amigo);
+					if(pos !== -1) {
+						this.listaDeudas[pos].agregarDeuda(nombre, monto);
+					} else {
+						let nuevaDeuda = new Deuda(amigo, [nombre], [monto]);
+						this.listaDeudas.push(nuevaDeuda);
+					}
+				}
+			}
+		} else {
+			for (let amigo in this.getListaIntegrantes()) {
+				let nombreAmigo = amigo.getNombre();
+				if(nombreAmigo !== nombre) {
+					let pos = this.getDeudaDe(nombreAmigo);
+					if(pos !== -1) {
+						this.listaDeudas[pos].agregarDeuda(nombre, monto);
+					} else {
+						let nuevaDeuda = new Deuda(nombreAmigo, [nombre], [monto]);
+						this.listaDeudas.push(nuevaDeuda);
+					}
+				}
+			}
+		}
 	}
 
 	balancear() {
-		// for (let deuda in this.lista) {
-		// 	var monto = gasto.getMonto() / gasto.getCantidadParticipantes();
-		// 	var dueño = gasto.getDueño();
-		// 	for (let p in gasto.getParticipantes() {
-		// 		let nombre = p.getNombre();
-		// 		if(!nombre === (dueño.getNombre())){
-		// 			let indexDeuda = this.getDeudaDe(nombre);
-		// 			if(indexDeuda =! -1) {
-		// 				this.listaDeudas.get(indexDeuda).agregarDeuda(dueño, monto);
-		// 			} else {
-		// 				var nuevaDeuda = Deuda(nombre);
-		// 				nuevaDeuda.agregarDeuda(dueño, monto);
-		// 				this.listaDeudas.push(nuevaDeuda);
-		// 			}
-		// 		}
-		// 	}
-		// }
+		for(let deuda1 in this.listaDeudas){
+			let amigos = deuda1.getAmigos();
+			let montos = deuda1.getMontos();
+			for(let i = 0; i < amigos.length; i++) {
+				let amigo = amigos[i];
+				let monto = monto[i];
+				let pos = this.getDeudaDe(amigo);
+				if(pos !== -1) {
+					let deuda2 = this.listaDeudas[pos];
+					let amigos2 = deuda2.getAmigos();
+					let montos2 = deuda2.getMontos();
+					for(let j = 0; j < amigos2.length; j++) {
+						if(monto > montos2[j]) {
+							monto[i] -= montos2[j];
+							deuda1.agregarDeuda(amigos2[j], montos2[j]);
+							deuda2.eliminarDeuda(j);
+						} else if(monto < montos2[j]){
+							deuda1.agregarDeuda(amigos2[j], monto);
+							deuda1.eliminarDeuda(i);
+							deuda2.modificarDeuda(j, montos2[j] - monto);
+						}
+						else {
+							deuda1.agregarDeuda(amigos2[j], montos2[j]);
+							deuda2.eliminarDeuda(j);
+						}
+					}
+				} 
+			}
+		}
 	}
 
 }
