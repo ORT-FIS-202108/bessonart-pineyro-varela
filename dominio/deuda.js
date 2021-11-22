14ef9f295342bba2828ca594a00b487e0408a075
FlorenciaV <florencia.v.peguri@gmail.com>
2021-11-22 17:44:54 -0300

    fix:grupo.test.js

--- dominio/deuda.js
+++ dominio/deuda.js
@@ -1,5 +1,5 @@
 class Deuda {
-	constructor(nombre,id) {
+	constructor(nombre, id) {
 		this.nombre = nombre;
 		this.amigos = [];
 		this.montos = [];
@@ -18,21 +18,21 @@ class Deuda {
 	}
 
 	agregarDeuda(amigo, monto) {
-        if(this.amigos.includes(amigo)) {
-            this.montos[this.amigos.indexOf(amigo)] += monto;
-        } else {
-            this.amigos.push(amigo);
-		    this.montos.push(monto);
-        }		
+		if (this.amigos.includes(amigo)) {
+			this.montos[this.amigos.indexOf(amigo)] += monto;
+		} else {
+			this.amigos.push(amigo);
+			this.montos.push(monto);
+		}
 	}
 
-    eliminarDeuda(posicion) {
-        this.amigos.splice(posicion, 1);
-        this.montos.splice(posicion, 1);
-    }
+	eliminarDeuda(posicion) {
+		this.amigos.splice(posicion, 1);
+		this.montos.splice(posicion, 1);
+	}
 
-    modificarDeuda(posicion, monto) {
-        this.montos[posicion] = monto;
-    }
+	modificarDeuda(posicion, monto) {
+		this.montos[posicion] = monto;
+	}
 }
 export default Deuda;
