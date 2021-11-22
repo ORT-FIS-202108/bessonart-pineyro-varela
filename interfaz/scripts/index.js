5408bd37bfc4ee99c5e723ebc886f6de88fdbe31
SofIa Piñeyro <sofiapineyro2302@hotmail.com>
2021-11-22 16:56:06 -0300

    Fix: aparece boton agregar gasto despues de seleccionar un grupo

--- interfaz/scripts/index.js
+++ interfaz/scripts/index.js
@@ -391,7 +391,7 @@ function saldarDeuda(nombre, amigo) {
 const grupoSeleccionado =((grupo) => {
 	grupoActivo = grupo;
 	document.querySelector('#nombreGrupo').innerHTML = grupo.getNombre();
-	document.querySelector('#botonAgregarGasto').classList.remove('sample-content--hidden');
+	document.querySelector('.mdc-touch-target-wrapper').classList.remove('sample-content--hidden');
 	mostrarDeudas();
 });
 
